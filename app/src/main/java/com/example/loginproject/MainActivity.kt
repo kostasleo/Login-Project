d828ea20e2e954f86cd5aeeb93ed72ef403324af
Konstantinos Leontaridis <kon.leontaridis@gmail.com>
2022-10-23 01:31:59 +0300

    login ui done

--- app/src/main/java/com/example/loginproject/MainActivity.kt
+++ app/src/main/java/com/example/loginproject/MainActivity.kt
@@ -1,40 +1,60 @@
 package com.example.loginproject
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.textInputServiceFactory
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.loginproject.ui.theme.DarkGrayMe
 import com.example.loginproject.ui.theme.LoginProjectTheme
+import com.example.loginproject.ui.theme.MyColors
+import android.content.Context
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.layout.HorizontalAlignmentLine
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import com.example.loginproject.ui.theme.GreenMe
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var notLoggedIn = false
+
         setContent {
             LoginProjectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background
-                    color = Color.Green
+                    color = MaterialTheme.colors.background
                 ) {
-                    LoginPage()
+                    if(notLoggedIn) {
+                        LoginPage(context = applicationContext)
+                    } else {
+                        HomePage()
+                    }
                 }
             }
         }
@@ -42,55 +62,164 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun LoginPage() {
-    var userId by remember { mutableStateOf("Πληκτρολογήστε το User ID σας") }
+fun HomePage() {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        TopLabel(type = "home")
+    }
+}
+
+@Composable
+fun LoginPage(colors: TextFieldColors = MyColors(), context: Context) {
+    var userId by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false)}
 
     Column(modifier = Modifier.fillMaxWidth()) {
-        Surface(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(74.dp),
-            color = Color.Black,
-        ) {
-            Text(
-                modifier = Modifier
-                    .height(41.dp)
-                    .padding(top = 20.dp),
-                text = "Σύνδεση",
-                fontSize = 28.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.White,
-                textAlign = TextAlign.Center,
-            )
-        }
+
+        TopLabel("login")
 
         Surface(
             modifier = Modifier
                 .fillMaxSize(),
-            color = Color(0xFF111111)
+            color = DarkGrayMe
         ) {
-//            Image(painter = painterResource(R.drawable.bg_gradient), contentDescription = null)
-            Column(modifier = Modifier.padding(20.dp)) {
-                Spacer(modifier = Modifier.height(51.dp))
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(38.dp)
+            ) {
+                Spacer(modifier = Modifier.height(71.dp))
+
+                Row(modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically){
+                InputLabelWithInfo(type = stringResource(R.string.user_id_label))}
+
                 TextField(
                     value = userId,
-                    onValueChange = { userId = it },
-                    label = { Text("UserID", color = Color.Gray, fontSize = 24.sp) },
+                    onValueChange = {userId = it},
+                    placeholder = { Text(text = "Πληκτρολογήστε το UserID σας", color = Color.Gray)},
+                    singleLine = true,
                     modifier = Modifier
+                        .background(DarkGrayMe)
                         .fillMaxWidth()
-                        .padding(bottom = 10.dp, top = 10.dp)
-                        .background(Color(0xFF111111))
-                    )
+                        .height(52.dp),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    colors = colors
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Row(modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    InputLabelWithInfo(type = stringResource(R.string.password_label_gr))
+                    TextButton(onClick = { passwordVisible = !passwordVisible },
+                        modifier = Modifier.padding(2.dp)) {
+                        Text(
+                            text = stringResource(R.string.show_password_label_gr),
+                            style = TextStyle(textAlign = TextAlign.End),
+                            color = GreenMe,
+                        )
+                    }
+                }
+                TextField(
+                    value = password,
+                    onValueChange = {password = it},
+                    placeholder = { Text(text = "Πληκτρολογήστε τον Κωδικό σας", color = Color.Gray)},
+                    singleLine = true,
+                    modifier = Modifier
+                        .background(DarkGrayMe)
+                        .fillMaxWidth()
+                        .height(52.dp),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                    colors = colors,
+                    visualTransformation = if(passwordVisible) VisualTransformation.None else PasswordVisualTransformation()
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                OutlinedButton(
+                    onClick = { handleLogin(userId, password, context) },
+                    border = BorderStroke(1.dp, GreenMe),
+                    shape = RoundedCornerShape(50),
+                    colors = ButtonDefaults.outlinedButtonColors(contentColor = GreenMe),
+                    modifier = Modifier
+                        .width(200.dp)
+                        .height(50.dp)
+                ){
+                    Text( text = stringResource(R.string.login_text_gr),
+                            fontSize = 20.sp)
+                }
             }
         }
     }
 }
 
+fun handleLogin(userId: String, password: String, context: Context) {
+    if (userId == "1234" && password == "1234"){
+        Toast.makeText(context, "Logged In Successfully", Toast.LENGTH_SHORT).show()
+    } else {
+        Toast.makeText(context, "Log In Failed", Toast.LENGTH_SHORT).show()
+    }
+}
+
+@Composable
+fun TopLabel(type:String){
+    var text:String = stringResource(R.string.login_text_gr)
+    if(type == "home"){
+        text = stringResource(R.string.home_text_label)
+    }
+    
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(74.dp),
+        color = Color.Black,
+    ) {
+        Text(
+            modifier = Modifier
+                .height(41.dp)
+                .padding(top = 20.dp),
+            text = text,
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Composable
+fun InputLabelWithInfo(type: String) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+
+        Text(
+            text = type, color = Color.White, fontSize = 22.sp,
+            style = TextStyle(textAlign = TextAlign.Start),
+            modifier = Modifier.padding(start = 10.dp),
+        )
+
+        IconButton(onClick = {}
+//                  onClick = { Toast.makeText(context, "Info", Toast.LENGTH_SHORT).show()}
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_info),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(20.dp)
+                    .width(20.dp)
+            )
+        }
+    }
+}
+
+
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     LoginProjectTheme {
-        LoginPage()
+        LoginPage(context = LocalContext.current)
     }
 }
--- app/src/main/java/com/example/loginproject/ui/theme/Theme.kt
+++ app/src/main/java/com/example/loginproject/ui/theme/Theme.kt
@@ -2,20 +2,24 @@ package com.example.loginproject.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
+    primary = GreenMe,
     primaryVariant = Purple700,
     secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
+    primary = GreenMe,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = DarkGrayMe,
+    surface = DarkGrayMe
 
     /* Other default colors to override
     background = Color.White,
@@ -42,3 +46,13 @@ fun LoginProjectTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Comp
         content = content
     )
 }
+
+@Composable
+fun MyColors (
+    textColor: Color = Color.White,
+    disabledTextColor: Color = GreenMe,
+    backgroundColor: Color = DarkGrayMe
+) = TextFieldDefaults.textFieldColors(
+    textColor = textColor,
+    backgroundColor = backgroundColor
+)
